9102d74b3220f8692a21ad3bcca83ea8eb5cfd8a
Deepankar <deepankar080892@gmail.com>
2020-08-09 21:33:12 +0530

    Basic component setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+
+import Header from './components/Header';
+import Figure from './components/Figure';
+import WrongLetters from './components/WrongLetters';
+import Word from './components/Word';
+import Popup from './components/Popup';
+import Notification from './components/Notification';
 import './App.css';
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Header />
+      <div className="game-container">
+        <Figure />
+        <WrongLetters />
+        <Word />
+      </div>
+      <Popup />
+      <Notification />
+    </div>
+  );
 }
 
 export default App;
